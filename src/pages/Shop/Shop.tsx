a2683caed5057b5d1bfc1eed1ef32b94131990d2
ModernDesigners <120728081+AtukaJikuridze@users.noreply.github.com>
2024-06-19 19:05:20 +0400

    Added shop filter coin/usd market

--- src/pages/Shop/Shop.tsx
+++ src/pages/Shop/Shop.tsx
@@ -1,7 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import priceTag from "../../assets/PriceTag.svg";
 import dComp1 from "../../assets/dashboardComponent1.svg";
 import Card from "./components/Card";
+import { API } from "../../baseAPI";
+
 export default function Shop() {
+  interface CardInterface {
+    product_image: string | null; // Assuming product_image is a string URL
+    product_name: string;
+    product_price_in_usd?: number;
+    product_price_in_coin?: number;
+  }
+
+  const [shopItems, setShopItems] = useState<CardInterface[] | null>(null);
+
+  useEffect(() => {
+    axios
+      .get(`${API}/market`)
+      .then((res) => setShopItems(res.data))
+      .catch((error) => console.error("Error fetching shop items:", error));
+  }, []);
+
+  console.log(shopItems);
+
   return (
     <div className="myContainer">
       <div className="flex gap-[2%] gap-y-10 flex-wrap">
@@ -13,7 +35,7 @@ export default function Shop() {
               <img src={priceTag} alt="" />
               <p>100 Coin</p>
             </div>
-            <div className="flex gap-5 ">
+            <div className="flex gap-5">
               <button className="text-black bg-yellowButton p-3 rounded-md px-5">
                 Buy
               </button>
@@ -23,18 +45,19 @@ export default function Shop() {
             </div>
           </div>
         </div>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {shopItems &&
+          shopItems.map((item) => (
+            <Card
+              key={item.product_name} // Assuming product_name is unique
+              product_image={item.product_image}
+              product_name={item.product_name}
+              product_price_in_usd={item.product_price_in_usd}
+              product_price_in_coin={item.product_price_in_coin}
+            />
+          ))}
       </div>
     </div>
   );
 }
+
+
--- src/pages/Shop/components/Card.tsx
+++ src/pages/Shop/components/Card.tsx
@@ -1,7 +1,30 @@
-export default function Card() {
+interface CardProps {
+  product_image: string | null;
+  product_name: string;
+  product_price_in_usd?: number;
+  product_price_in_coin?: number;
+}
+
+export default function Card({
+  product_image,
+  product_name,
+  product_price_in_usd,
+  product_price_in_coin,
+}: CardProps) {
   return (
-    <div className="w-[23%] bg-cardBgBlack rounded-md h-[500px] flex justify-center items-center">
-      Card
+    <div className="w-[23%] bg-cardBgBlack rounded-md h-[500px] flex flex-col justify-center items-center">
+      <img
+        src={product_image ?? ""}
+        alt={product_name}
+        className="w-full h-1/2 object-cover"
+      />
+      <h1>Title: {product_name}</h1>
+      <h1>
+        Product Price:{" "}
+        {product_price_in_usd
+          ? `${product_price_in_usd} "USD"`
+          : `${product_price_in_coin} Coin`}
+      </h1>
     </div>
   );
 }
